289c75c06ab792908622447058b9ae73a94fe993
UsamaKhan123456789 <usamaiftikhar75@gmail.com>
2024-10-08 20:55:44 +0500

    5th commit

--- lib/constants/app_textstyle.dart
+++ lib/constants/app_textstyle.dart
@@ -5,10 +5,10 @@ import '../presentation/widgets/app_colors.dart';
 class AppTextStyle {
   ///---------Font: Montserrat -------------------///
   static const pageTitleStyle = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.w700,
-    color: AppColor.black,
-    fontFamily: AppFontFamilies.Baloo2,
+      fontSize: 18,
+      fontWeight: FontWeight.w700,
+      color: AppColor.black,
+      fontFamily: 'Outfit'
   );
 
   static const buttonStyle = TextStyle(
@@ -41,19 +41,19 @@ class AppTextStyle {
     color: AppColor.dontHaveAnAccount,
     fontFamily: 'Inter',
   );
-static const signUpText = TextStyle(
+  static const signUpText = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w600,
     color: AppColor.signUpColor,
     fontFamily: 'Inter',
   );
-static const headingText = TextStyle(
+  static const headingText = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w500,
     color: AppColor.black,
     fontFamily: 'Outfit',
   );
-static const discountText = TextStyle(
+  static const discountText = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w500,
     color: AppColor.discountCardColor,
@@ -61,91 +61,33 @@ static const discountText = TextStyle(
   );
 
 
-  static const textButtonStyle = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.w600,
-    color: AppColor.primaryColor,
-    fontFamily: 'Montserrat',
-    decoration: TextDecoration.underline,
-    decorationColor: AppColor.primaryColor
-  );
-
-
   static const forgotPassword = TextStyle(
-    fontFamily: "Inter",
-    fontSize: 12,
-    color: AppColor.forgotColor
+      fontFamily: "Inter",
+      fontSize: 12,
+      color: AppColor.forgotColor
   );
   static const checkBoxText = TextStyle(
-    fontFamily: "Inter",
-    fontSize: 12,
-    color: AppColor.hintColor
+      fontFamily: "Inter",
+      fontSize: 12,
+      color: AppColor.hintColor
   );
 
   static const labelStyle = TextStyle(
       fontSize: 18,
       fontWeight: FontWeight.w400,
-      color: const Color(0xff444444),
-      fontFamily: AppFontFamilies.Baloo2
+      color: Color(0xff444444),
+      fontFamily: 'Outfit'
   );
 
   static const hintStyle = TextStyle(
-    fontSize: 18,
-    color: AppColor.hintColor,
-      fontFamily: AppFontFamilies.Baloo2
-  );
-
-  static const tableStyle = TextStyle(
-    fontSize: 12,
-    fontWeight: FontWeight.w500,
-    // color: AppColor.dateColor,
-    fontFamily: AppFontFamilies.montserrat,
-  );
-  ///
-  static const filedTextStyle = TextStyle(
       fontSize: 18,
-      fontWeight: FontWeight.w400,
-      color: const Color(0xff000000 ),
-      fontFamily: AppFontFamilies.Baloo2
+      color: AppColor.hintColor,
+      fontFamily: 'Outfit'
   );
-  ///---------Font: SFProRounded -------------------///
   static const infoStyle = TextStyle(
-    fontSize: 25,
-    fontWeight: FontWeight.w600,
-    color:  AppColor.black,
-    fontFamily: 'Inter'
+      fontSize: 25,
+      fontWeight: FontWeight.w600,
+      color: AppColor.black,
+      fontFamily: 'Inter'
   );
-
-  static const errorStyle = TextStyle(
-  fontSize: 18,
-  fontWeight: FontWeight.w500,
-  color: AppColor.black,
-  fontFamily: AppFontFamilies.sf_pro,
-  );
-
-  ///---------Font: Inter -------------------///
-  static const disclaimerStyle = TextStyle(
-    fontSize: 10.5,
-    fontWeight: FontWeight.w400,
-    color: AppColor.black,
-    fontFamily: AppFontFamilies.Baloo2,
-  );
-
-  static const loadingStyle = TextStyle(
-    fontSize: 18.5,
-    fontWeight: FontWeight.w500,
-    color: AppColor.black,
-    letterSpacing: -0.37,
-    fontFamily: AppFontFamilies.Baloo2,
-  );
-}
-
-
-///-------------------App Font Families -------------------///
-class AppFontFamilies
-{
-  static const String Baloo2 = 'Baloo2';
-  static const String montserrat = 'Montserrat';
-  static const String sf_pro = 'SFProRounded';
-  static const String raleway = 'Raleway';
 }
